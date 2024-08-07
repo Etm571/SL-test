61879e3f704d209f240645297865aa9561d6daad
Bo Gustavsson <bosse@etm571.com>
2024-08-07 21:10:31 +0200

    autofill + style

--- public/script.js
+++ public/script.js
@@ -1,20 +1,23 @@
+document.getElementById('fromInput').addEventListener('input', function() {
+    nuvarandeBox = 'frånPlats';
+    searchAPI();
+});
+
+document.getElementById('toInput').addEventListener('input', function() {
+    nuvarandeBox = 'tillPlats';
+    searchAPI();
+});
 
 async function searchAPI() {
     const frånPlats = document.getElementById('fromInput').value;
     const tillPlats = document.getElementById('toInput').value;
-    var nuvarandeBox = "";
-    if (frånPlats == "")    {
-        nuvarandeBox = tillPlats;
-    }
-    else{
-        nuvarandeBox = frånPlats;
-    }
+    
     const response = await fetch('/api/search', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ nuvarandeBox })
+        body: JSON.stringify({ nuvarandeBox: nuvarandeBox == 'frånPlats' ? frånPlats : tillPlats })
     });
 
     if (!response.ok) {
@@ -28,13 +31,22 @@ async function searchAPI() {
     const resultsDiv = document.getElementById('items');
     resultsDiv.innerHTML = "";
 
-    if (nuvarandeBox == "") {
+    if ((nuvarandeBox == 'frånPlats' && frånPlats == "") || (nuvarandeBox == 'tillPlats' && tillPlats == "")) {
         resultsDiv.innerHTML = "";
     } else {
-        const nuvarandeBox = data.ResponseData.map(place => place.Name);
-        nuvarandeBox.forEach(name => {
+        const nuvarandeBoxArray = data.ResponseData.map(place => place.Name);
+        nuvarandeBoxArray.forEach(name => {
             const p = document.createElement('p');
             p.textContent = name;
+            p.addEventListener('click', () => {
+                if (nuvarandeBox == 'tillPlats') {
+                    document.getElementById('toInput').value = name;
+                    resultsDiv.innerHTML = "";
+                } else {
+                    document.getElementById('fromInput').value = name;
+                    resultsDiv.innerHTML = "";
+                }
+            });
             resultsDiv.appendChild(p);
         });
     }
